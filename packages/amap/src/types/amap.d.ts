5f5bb714c8083e731cb3cc353d8788225c6f9f26
cyh <2419068625@qq.com>
2023-02-22 14:27:17 +0800

    build: update

--- packages/amap/src/types/amap.d.ts
+++ packages/amap/src/types/amap.d.ts
@@ -1,14 +1,5 @@
 /// <reference types="@amap/amap-jsapi-types" />
 declare namespace AMapLoader {
-    /**
-     * @param key '申请好的Web端开发者Key，首次调用 load 时必填'
-     * @param version '指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15'
-     * @param plugins '需要使用的的插件列表，如比例尺'AMap.Scale'等'
-     * @param AMapUI '是否加载 AMapUI，缺省不加载'
-     * @param Loca '是否加载 Loca， 缺省不加载'
-     * @param serviceHost '您的代理服务器域名或地址/_AMapService'
-     * @param securityJsCode '您申请的安全密钥'
-     */
     interface LoaderOptions {
       key: string;
       version: string;
@@ -23,7 +14,15 @@ declare namespace AMapLoader {
       serviceHost?: string,
       securityJsCode?: string
     }
-  
+    /**
+     * @param key '申请好的Web端开发者Key，首次调用 load 时必填'
+     * @param version '指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15'
+     * @param plugins '需要使用的的插件列表，如比例尺'AMap.Scale'等'
+     * @param AMapUI '是否加载 AMapUI，缺省不加载'
+     * @param Loca '是否加载 Loca， 缺省不加载'
+     * @param serviceHost '您的代理服务器域名或地址/_AMapService'
+     * @param securityJsCode '您申请的安全密钥'
+     */
     function load(options: LoaderOptions): Promise<void>;
   }
   
